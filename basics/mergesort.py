06b7cdb80ca4f76e79dfa81d5dc038fccb149914
Nella Mantyniemi <nella.mantyniemi@tuni.fi>
2023-01-10 13:54:06 +0200

    Added debug messages

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,11 @@
-  print("{}: {}".format(key, value))
+
+def debug_print(debug_msg=None, **kwargs):
+
+    if debug_msg:
+        print(debug_msg)
+
+    for key, value in kwargs.items():
+        print("{}: {}".format(key, value))
 
 
 def mergesort(array):
@@ -7,7 +14,6 @@ def mergesort(array):
         return array
 
     m = len(array) // 2
-    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -30,18 +36,22 @@ def merge(left, right):
         merged += left
     else:
         merged += right
-
+    
     debug_print(merged=merged)
+
     return merged
 
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-
+    input_list = input_str.split(",")
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
             quit(1)
-
-    debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
-
